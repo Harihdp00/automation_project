d060b593f30d94944661ae3583897562d2b7cc4f
Harihdp00 <harihdp00@gmail.com>
2025-11-08 19:19:34 +0530

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,30 @@
+############################################################
+# PROVIDER
+############################################################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
